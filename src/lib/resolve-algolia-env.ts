eb6053ce8277b88c2dabdb6cccf0ef33e934ec75
Prashant Singh <psingh@orkutics.com>
2024-09-27 15:31:41 +0100

    fix: default value if algolia is false

--- src/lib/resolve-algolia-env.ts
+++ src/lib/resolve-algolia-env.ts
@@ -1,10 +1,18 @@
 export const algoliaEnvData = algoliaEnv();
 
-function algoliaEnv(): { appId: string; apiKey: string; indexName: string, enabled: boolean } {
+function algoliaEnv(): {
+  appId: string;
+  apiKey: string;
+  indexName: string;
+  enabled: boolean;
+} {
   const appId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID;
   const apiKey = process.env.NEXT_PUBLIC_ALGOLIA_API_KEY;
   const indexName = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME;
-  const enabled = process.env.NEXT_PUBLIC_ENABLE_ALGOLIA === "true"
+  const enabled = process.env.NEXT_PUBLIC_ENABLE_ALGOLIA === "true";
+  if (!enabled) {
+    return { appId: "", apiKey: "", indexName: "", enabled };
+  }
   if (!appId || !apiKey || !indexName) {
     throw new Error(
       `Failed to get algolia search environment variables. \n Make sure you have set NEXT_PUBLIC_ALGOLIA_APP_ID, NEXT_PUBLIC_ALGOLIA_API_KEY and NEXT_PUBLIC_ALGOLIA_INDEX_NAME`,
