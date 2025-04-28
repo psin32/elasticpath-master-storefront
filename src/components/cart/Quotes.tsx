7d1d507d16d57117fe362f11ef88c35b3bfc2474
Robert Field <robertfield@quadrimular.com>
2025-04-28 13:13:20 +0100

    fix: add col span to cover both cols

--- src/components/cart/Quotes.tsx
+++ src/components/cart/Quotes.tsx
@@ -200,6 +200,7 @@ export function Quotes({ account }: QuotesProps) {
                         </th>
                         <th
                           scope="col"
+                          colSpan={2}
                           className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                         >
                           Actions
