02348fb6267fd7f2ff74599237fd5aa54e76cbfb
Robert Field <robertfield@quadrimular.com>
2025-04-28 12:08:00 +0100

    fix: checkout buttons

--- src/components/cart/Quotes.tsx
+++ src/components/cart/Quotes.tsx
@@ -296,22 +296,32 @@ export function Quotes({ account }: QuotesProps) {
                             </span>
                           </td>
                           <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                            <div className="flex space-x-2">
-                              <button
-                                className="text-xs bg-gray-100 hover:bg-gray-200 text-gray-900 font-bold py-2 px-4 rounded"
-                                onClick={() =>
-                                  downloadPDF(quote.id, quote.quote_ref)
-                                }
-                              >
-                                Download PDF
-                              </button>
-                              <Link
-                                href="/checkout"
-                                className="text-xs bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded inline-block"
+                            {quote.status === "Approved" && (
+                              <Link href={`/quote-checkout/${quote.quote_ref}`}>
+                                <StatusButton className="text-sm rounded-lg">
+                                  Checkout
+                                </StatusButton>
+                              </Link>
+                            )}
+                            {quote.status != "Approved" && (
+                              <StatusButton
+                                className="text-sm rounded-lg"
+                                disabled
                               >
                                 Checkout
-                              </Link>
-                            </div>
+                              </StatusButton>
+                            )}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            <StatusButton
+                              className="text-sm rounded-lg"
+                              disabled={quote.status === "Completed"}
+                              onClick={() =>
+                                downloadPDF(quote.id, quote.quote_ref)
+                              }
+                            >
+                              Download PDF
+                            </StatusButton>
                           </td>
                         </tr>
                       ))}
