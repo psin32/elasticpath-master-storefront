f85a05aaaf20beede8b595fc975e46e4f30a2343
Prashant Singh <psingh@orkutics.com>
2025-06-30 14:05:53 +0100

    chore: change quantity input on cart so that user can give the quantity

--- src/components/number-input/NumberInput.tsx
+++ src/components/number-input/NumberInput.tsx
@@ -1,12 +1,39 @@
 "use client";
 import type { CartItem } from "@elasticpath/js-sdk";
 import { EditItemQuantityButton } from "./EditQuantityButton";
+import { useState } from "react";
+import { useCart } from "../../react-shopper-hooks";
 
 interface NumberInputProps {
   item: CartItem;
 }
 
 export const NumberInput = ({ item }: NumberInputProps): JSX.Element => {
+  const { useScopedUpdateCartItem } = useCart();
+  const { mutate, isPending } = useScopedUpdateCartItem();
+  const [inputValue, setInputValue] = useState(item.quantity.toString());
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.value;
+    // Only allow numbers
+    if (/^\d*$/.test(val)) {
+      setInputValue(val);
+    }
+  };
+
+  const handleInputBlurOrEnter = () => {
+    const newQuantity = parseInt(inputValue, 10);
+    if (
+      !isNaN(newQuantity) &&
+      newQuantity > 0 &&
+      newQuantity !== item.quantity
+    ) {
+      mutate({ itemId: item.id, quantity: newQuantity });
+    } else {
+      setInputValue(item.quantity.toString()); // Reset if invalid
+    }
+  };
+
   return (
     <div className="flex items-start rounded-lg border border-black/10">
       <EditItemQuantityButton item={item} type="minus" />
@@ -25,10 +52,32 @@ export const NumberInput = ({ item }: NumberInputProps): JSX.Element => {
           strokeLinejoin="round"
         />
       </svg>
-
-      <span className="flex w-9 pt-1.5 pb-1 border-none justify-center">
-        {item.quantity}
-      </span>
+      <input
+        type="number"
+        min={1}
+        className="flex w-20 pt-1.5 pb-1 border-none text-center bg-transparent focus:outline-none appearance-none hide-number-spin"
+        value={inputValue}
+        onChange={handleInputChange}
+        onBlur={handleInputBlurOrEnter}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            (e.target as HTMLInputElement).blur();
+          }
+        }}
+        disabled={isPending}
+        aria-label="Cart item quantity"
+        style={{ MozAppearance: "textfield" }}
+      />
+      <style jsx global>{`
+        input[type="number"].hide-number-spin::-webkit-inner-spin-button,
+        input[type="number"].hide-number-spin::-webkit-outer-spin-button {
+          -webkit-appearance: none;
+          margin: 0;
+        }
+        input[type="number"].hide-number-spin {
+          -moz-appearance: textfield;
+        }
+      `}</style>
       <svg
         width="2"
         height="36"
